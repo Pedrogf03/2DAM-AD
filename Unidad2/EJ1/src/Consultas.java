9b275a54e9f308cf948b30475c4577c811ac4ab5
Pedrogf03 <bleikerpro23@gmail.com>
2023-11-01 13:49:17 +0100

    1 11 23

---
+++ Unidad2/EJ1/src/Consultas.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class Consultas {
+
+  // ---------------------- SQLite ----------------------
+  static String driver = "org.sqlite.JDBC";
+  static String url = "jdbc:sqlite:C:/Users/bleik/Desktop/sqlite/ejemplo.db";
+
+  // ---------------------- MySQL -----------------------
+  // static String driver="com.mysql.cj.jdbc.Driver";
+  // static String url="jdbc:mysql://localhost/empresa";
+
+  // ---------------- Parámetros de BBDD ----------------
+  static String db_drivers = driver;
+  static String db_user = "root";
+  static String db_passwd = "123456";
+
+  public static void main(String[] args) {
+    
+    String consulta = "SELECT"
+
+  }
+
+}
