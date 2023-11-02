3b4ece9e184f2a382e0ffffab85ecc1d762a7122
Pedrogf03 <bleikerpro23@gmail.com>
2023-11-02 10:40:24 +0100

    2 11 23

--- Unidad1/EJ6/src/borradoSeguro.java
+++ Unidad1/EJ6/src/borradoSeguro.java
@@ -2,58 +2,59 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class borradoSeguro {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        try {
+    try {
 
-            if(args.length == 0){
-                throw new LineaComandosException();
-            } else {
-                
-                File papelera = new File(".\\papelera");
+      if (args.length == 0) {
+        throw new LineaComandosException();
+      } else {
 
-                if(!papelera.exists()) {
-                    papelera.mkdir();
-                }
+        File papelera = new File(".\\papelera");
 
-                for(int i = 0; i < args.length; i++){
-
-                    File f = new File(args[i]);
-
-                    try {
-                        if(!f.exists()) {
-                            throw new FileNotFoundException();
-                        }
-                    } catch (FileNotFoundException e){
-                        System.out.println("No se ha encontrado el archivo o directorio " + f.getName());
-                    }
-
-                    File destino = new File(papelera, f.getName());
-
-                    int cont = 1;
-                    while (destino.exists()){
-
-                        String extensionArchivo = destino.getName().substring(destino.getName().lastIndexOf(".") + 1);
-
-                        File temp = new File(papelera, destino.getName().substring(0, destino.getName().lastIndexOf(".")) + "(" + cont + ")." + extensionArchivo);
-
-                        if(temp.exists()){
-                            cont++;
-                        } else {
-                            destino = temp;
-                        }
-
-                    }
-
-                    f.renameTo(destino);
-
-                }
-
-            }
-
-        } catch (LineaComandosException e) {
-            System.out.println("Indique los archivos o directorios que desea eliminar.");
+        if (!papelera.exists()) {
+          papelera.mkdir();
         }
 
+        for (int i = 0; i < args.length; i++) {
+
+          File f = new File(args[i]);
+
+          try {
+            if (!f.exists()) {
+              throw new FileNotFoundException();
+            }
+          } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el archivo o directorio " + f.getName());
+          }
+
+          File destino = new File(papelera, f.getName());
+
+          int cont = 1;
+          while (destino.exists()) {
+
+            String extensionArchivo = destino.getName().substring(destino.getName().lastIndexOf(".") + 1);
+
+            File temp = new File(papelera, destino.getName().substring(0, destino.getName().lastIndexOf(".")) + "("
+                + cont + ")." + extensionArchivo);
+
+            if (temp.exists()) {
+              cont++;
+            } else {
+              destino = temp;
+            }
+
+          }
+
+          f.renameTo(destino);
+
+        }
+
+      }
+
+    } catch (LineaComandosException e) {
+      System.out.println("Indique los archivos o directorios que desea eliminar.");
     }
+
+  }
 }
