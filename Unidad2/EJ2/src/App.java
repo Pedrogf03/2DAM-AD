da9b934f61aca99b7fa2f9a136a43b566c31f0b3
Pedrogf03 <bleikerpro23@gmail.com>
2023-11-06 08:35:10 +0100

    6 11 23

--- Unidad2/EJ2/src/App.java
+++ Unidad2/EJ2/src/App.java
@@ -17,46 +17,58 @@ public class App {
 
   public static void main(String[] args) throws Exception {
 
-    int option1 = 0;
-    int option2 = 0;
+    int optionTable = 0;
+    int optionOp = 0;
     String tabla = "";
     Scanner sc = new Scanner(System.in);
 
-    do {
-
+    while (true) {
+      System.out.println("----------------------------------------------------------------------");
       System.out.println("Selecciona la tabla: ");
       System.out.println("1.- Departamentos.");
       System.out.println("2.- Empleados.");
       System.out.println("0.- Cerrar.");
 
-      option1 = Integer.parseInt(sc.nextLine());
+      optionTable = Integer.parseInt(sc.nextLine());
 
-      if (option1 == 1) {
+      if (optionTable == 0) {
+        break;
+      }
+
+      if (optionTable == 1) {
         tabla = "DEPARTAMENTOS";
-      } else if (option1 == 2) {
+      } else if (optionTable == 2) {
         tabla = "EMPLEADOS";
       }
 
-      while (!tabla.equals("")) {
-
+      while (true) {
+        System.out.println("----------------------------------------------------------------------");
         System.out.println("Selecciona la operación que quiere hacer con la tabla " + tabla);
         System.out.println("1.- Realizar alta.");
         System.out.println("2.- Realizar baja.");
         System.out.println("3.- Actualizar registros.");
         System.out.println("4.- Consultas.");
+        System.out.println("0.- Salir.");
 
-        switch (option2) {
+        optionOp = Integer.parseInt(sc.nextLine());
+
+        if (optionOp == 0) {
+          tabla = "";
+          break;
+        }
+
+        switch (optionOp) {
           case 1:
-
+            insertInto(tabla);
             break;
           case 2:
-
+            System.out.println("Bajas con la tabla " + tabla);
             break;
           case 3:
-
+            System.out.println("Actualizar la tabla " + tabla);
             break;
           case 4:
-
+            System.out.println("Consultar la tabla " + tabla);
             break;
           default:
             break;
@@ -64,9 +76,27 @@ public class App {
 
       }
 
-    } while (option1 != 0);
+    }
 
     sc.close();
 
   }
+
+  public static int insertInto(String tabla) {
+
+    switch (tabla) {
+      case "DEPARTAMENTOS":
+
+        //TODO: Insertar en tabla departamentos.
+
+      case "EMPLEADOS":
+
+        //TODO: Insertar en tabla empleados.
+
+      default:
+        return -1;
+    }
+
+  }
+
 }
