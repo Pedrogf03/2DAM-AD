5cfe6ed9b90619305f69461cc757a470c05131a5
Pedrogf03 <bleikerpro23@gmail.com>
2023-11-23 12:06:17 +0100

    23 11 23

--- Example/GestionEmpresaHibernate/src/company/entity/collectionsmappings/e_bag_one_to_many/Certificate.java
+++ Example/GestionEmpresaHibernate/src/company/entity/collectionsmappings/e_bag_one_to_many/Certificate.java
@@ -6,23 +6,29 @@ package company.entity.collectionsmappings.e_bag_one_to_many;
  *    can be stored and retrieved into the CERTIFICATE table.
  */
 public class Certificate {
-    private int id;
-    private String name;
-    
-    public Certificate() {} 
-    public Certificate(String name) {
-        this.name = name;
-    } 
-    public int getId() {
-        return id;
-    }
-    public void setId( int id ) {
-        this.id = id;
-    }
-    public String getName() { 
-        return name;
-    }
-    public void setName( String name ) {
-        this.name = name;
-    } 
+  private int id;
+  private String name;
+
+  public Certificate() {
+  }
+
+  public Certificate(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
--- Example/GestionEmpresaHibernate/src/company/main/collectionsmappings/e_bag_one_to_many/ManageEmployee.java
+++ Example/GestionEmpresaHibernate/src/company/main/collectionsmappings/e_bag_one_to_many/ManageEmployee.java
@@ -1,10 +1,8 @@
 package company.main.collectionsmappings.e_bag_one_to_many;
 
-
-        
 import java.util.List;
 import java.util.Iterator;
-import java.util.Collection;     //<-------------Collection
+import java.util.Collection; //<-------------Collection
 import java.util.ArrayList; //<-------------ArrayList
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -12,135 +10,136 @@ import org.hibernate.Transaction;
 
 import company.util.HibernateUtil;
 import company.entity.collectionsmappings.e_bag_one_to_many.*;
- 
+
 public class ManageEmployee {
 
-    public static void main(String[] args) {
-   
-        ManageEmployee ME = new ManageEmployee(); /* Add few employee records in database */
-        
-        /* Let us have a set of certificates for the first employee */
-        ArrayList set1 = new ArrayList();
-        set1.add(new Certificate("MCA"));
-        set1.add(new Certificate("MBA"));
-        set1.add(new Certificate("PMP")); 
-       /* Add employee records in the database */ 
-        Integer empID1 = ME.addEmployee("Manoj", "Kumar", 4000, set1);
-        
-        /* Another set of certificates for the second employee */
-        ArrayList set2 = new ArrayList(); set2.add(new Certificate("BCA"));
-        set2.add(new Certificate("BCA"));
-        set2.add(new Certificate("BA"));
-        
-        /* Add another employee record in the database */
-        Integer empID2 = ME.addEmployee("Dilip", "Kumar", 3000, set2);
-        
-        
-        /* List down all the employees */
-        ME.listEmployees();
-        
-        /* Update employee's records */
-        ME.updateEmployee(empID1, 5000);
-        
-        /* Delete an employee from the database */ 
-        ME.deleteEmployee(empID2);
-        
-        /* List down new list of the employees */
-        ME.listEmployees(); 
-    }
-        
-     
-    /* Method to add an employee record in the database */            //Collection
-    public Integer addEmployee(String fname, String lname, int salary, Collection cert){
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = null;
-        Integer employeeID = null;
+  public static void main(String[] args) {
 
-        try{
-            tx = session.beginTransaction();
-            Employee employee = new Employee(fname, lname, salary);
-            employee.setCertificates(cert);//<----------------------Collection
-            employeeID = (Integer) session.save(employee);
-            tx.commit();
-        }catch (HibernateException e) { 
-            if (tx!=null) tx.rollback();
-                e.printStackTrace();
-        }finally {
-            session.close();
-        } 
+    ManageEmployee ME = new ManageEmployee(); /* Add few employee records in database */
 
-        return employeeID; 
+    /* Let us have a set of certificates for the first employee */
+    ArrayList<Certificate> set1 = new ArrayList<>();
+    set1.add(new Certificate("MCA"));
+    set1.add(new Certificate("MBA"));
+    set1.add(new Certificate("PMP"));
+    /* Add employee records in the database */
+    Integer empID1 = ME.addEmployee("Manoj", "Kumar", 4000, set1);
+
+    /* Another set of certificates for the second employee */
+    ArrayList<Certificate> set2 = new ArrayList<>();
+    set2.add(new Certificate("BCA"));
+    set2.add(new Certificate("BCA"));
+    set2.add(new Certificate("BA"));
+
+    /* Add another employee record in the database */
+    Integer empID2 = ME.addEmployee("Dilip", "Kumar", 3000, set2);
+
+    /* List down all the employees */
+    ME.listEmployees();
+
+    /* Update employee's records */
+    ME.updateEmployee(empID1, 5000);
+
+    /* Delete an employee from the database */
+    ME.deleteEmployee(empID2);
+
+    /* List down new list of the employees */
+    ME.listEmployees();
+  }
+
+  /* Method to add an employee record in the database */ //Collection
+  public Integer addEmployee(String fname, String lname, int salary, Collection<Certificate> cert) {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    Transaction tx = null;
+    Integer employeeID = null;
+
+    try {
+      tx = session.beginTransaction();
+      Employee employee = new Employee(fname, lname, salary);
+      employee.setCertificates(cert);//<----------------------Collection
+      employeeID = (Integer) session.save(employee);
+      tx.commit();
+    } catch (HibernateException e) {
+      if (tx != null)
+        tx.rollback();
+      e.printStackTrace();
+    } finally {
+      session.close();
     }
 
-    /* Method to READ all the employees */ 
-    public void listEmployees( ){ 
-        Session session = HibernateUtil.getSessionFactory().openSession(); 
-        Transaction tx = null;
-       
-        try{
-            tx = session.beginTransaction();
-            List employees = session.createQuery("FROM Employee").list();
-            
-            //-------Iterate over the List obtanited from the Query
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
-                Employee employee = (Employee) iterator.next();
-                System.out.print("First Name: " + employee.getFirstName());
-                System.out.print(" Last Name: " + employee.getLastName());
-                System.out.println(" Salary: " + employee.getSalary());
-                
-                //-----Iterate over the Collection of Certificates of Employe i--------
-                Collection certificates = employee.getCertificates();
-                for (Iterator iterator2 = certificates.iterator();iterator2.hasNext();){
-                    Certificate certName = (Certificate) iterator2.next();
-                    System.out.println("Certificate: " + certName.getName());
-                } 
-            } 
-            tx.commit(); 
-        }catch (HibernateException e) { 
-            if (tx!=null) tx.rollback(); 
-            e.printStackTrace();
-        }finally {
-            session.close();
-        } 
-    }
+    return employeeID;
+  }
 
-    /* Method to UPDATE salary for an employee */
-    public void updateEmployee(Integer EmployeeID, int salary ){ 
+  /* Method to READ all the employees */
+  public void listEmployees() {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    Transaction tx = null;
 
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = null; 
-        try{ 
-            tx = session.beginTransaction(); 
-            Employee employee = (Employee)session.get(Employee.class, EmployeeID);
-            employee.setSalary( salary );
-            session.update(employee);
-            tx.commit();
-        }catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace(); 
-        }finally { 
-            session.close();
-        } 
-    }
+    try {
+      tx = session.beginTransaction();
+      @SuppressWarnings("unchecked")
+      List<Employee> employees = session.createQuery("FROM Employee").list();
 
-    /* Method to DELETE an employee from the records */ 
-    public void deleteEmployee(Integer EmployeeID){
+      //-------Iterate over the List obtanited from the Query
+      for (Iterator<Employee> iterator = employees.iterator(); iterator.hasNext();) {
+        Employee employee = (Employee) iterator.next();
+        System.out.print("First Name: " + employee.getFirstName());
+        System.out.print(" Last Name: " + employee.getLastName());
+        System.out.println(" Salary: " + employee.getSalary());
 
-        Session session = HibernateUtil.getSessionFactory().openSession(); 
-        Transaction tx = null;
-        try{
-            tx = session.beginTransaction(); 
-            Employee employee = (Employee)session.get(Employee.class, EmployeeID);
-            session.delete(employee);
-            tx.commit();
-        }catch (HibernateException e) { 
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        }finally {
-            session.close(); 
+        //-----Iterate over the Collection of Certificates of Employe i--------
+        Collection<Certificate> certificates = employee.getCertificates();
+        for (Iterator<Certificate> iterator2 = certificates.iterator(); iterator2.hasNext();) {
+          Certificate certName = (Certificate) iterator2.next();
+          System.out.println("Certificate: " + certName.getName());
         }
+      }
+      tx.commit();
+    } catch (HibernateException e) {
+      if (tx != null)
+        tx.rollback();
+      e.printStackTrace();
+    } finally {
+      session.close();
     }
+  }
+
+  /* Method to UPDATE salary for an employee */
+  public void updateEmployee(Integer EmployeeID, int salary) {
+
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    Transaction tx = null;
+    try {
+      tx = session.beginTransaction();
+      Employee employee = (Employee) session.get(Employee.class, EmployeeID);
+      employee.setSalary(salary);
+      session.update(employee);
+      tx.commit();
+    } catch (HibernateException e) {
+      if (tx != null)
+        tx.rollback();
+      e.printStackTrace();
+    } finally {
+      session.close();
+    }
+  }
+
+  /* Method to DELETE an employee from the records */
+  public void deleteEmployee(Integer EmployeeID) {
+
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    Transaction tx = null;
+    try {
+      tx = session.beginTransaction();
+      Employee employee = (Employee) session.get(Employee.class, EmployeeID);
+      session.delete(employee);
+      tx.commit();
+    } catch (HibernateException e) {
+      if (tx != null)
+        tx.rollback();
+      e.printStackTrace();
+    } finally {
+      session.close();
+    }
+  }
 }
-
-
-
