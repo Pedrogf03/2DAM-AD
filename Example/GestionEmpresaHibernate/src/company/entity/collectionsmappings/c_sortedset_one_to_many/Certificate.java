cfd3672b88a6e09055a4e7e954f6a7398ef54841
Pedrogf03 <bleikerpro23@gmail.com>
2023-11-23 11:58:39 +0100

    23 11 23

--- Example/GestionEmpresaHibernate/src/company/entity/collectionsmappings/c_sortedset_one_to_many/Certificate.java
+++ Example/GestionEmpresaHibernate/src/company/entity/collectionsmappings/c_sortedset_one_to_many/Certificate.java
@@ -8,49 +8,55 @@ package company.entity.collectionsmappings.c_sortedset_one_to_many;
  *    <code>compareTo</code> method which will be used to sort the elements in 
  *    case you set sort="natural" in your mapping file.
  */
-public class Certificate implements Comparable <Certificate> {//<----implementos 'Comparable'
-    private int id;
-    private String name;
-    
-    public Certificate() {} 
-    public Certificate(String name) {
-        this.name = name;
-    } 
-    public int getId() {
-        return id;
+public class Certificate implements Comparable<Certificate> {//<----implementos 'Comparable'
+  private int id;
+  private String name;
+
+  public Certificate() {
+  }
+
+  public Certificate(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * 
+   * @param that other Certificate to compare with
+   * @return -1 (BEFORE) if that is null, or its name is null,
+   *                     or this.name comes before that.name
+   *         +1 (AFTER)  this.name is null or this.name comes after that.name
+   */
+  public int compareTo(Certificate that) {
+    final int BEFORE = -1;
+    final int AFTER = 1;
+    if (that == null) {
+      return BEFORE;
     }
-    public void setId( int id ) {
-        this.id = id;
+    String thisCertificate = this.getName();
+    String thatCertificate = that.getName();
+
+    if (thisCertificate == null) {
+      return AFTER;
+    } else if (thatCertificate == null) {
+      return BEFORE;
+    } else {
+      return thisCertificate.compareTo(thatCertificate);
     }
-    public String getName() { 
-        return name;
-    }
-    public void setName( String name ) {
-        this.name = name;
-    }
-    
-    /**
-     * 
-     * @param that other Certificate to compare with
-     * @return -1 (BEFORE) if that is null, or its name is null,
-     *                     or this.name comes before that.name
-     *         +1 (AFTER)  this.name is null or this.name comes after that.name
-     */
-    public int compareTo(Certificate that){
-        final int BEFORE = -1;
-        final int AFTER = 1;
-        if (that == null) {
-            return BEFORE;
-        }
-        Comparable thisCertificate = this.getName();
-        Comparable thatCertificate = that.getName();
-        
-        if(thisCertificate == null) {
-            return AFTER;
-        } else if(thatCertificate == null) {
-                   return BEFORE;
-        } else {
-            return thisCertificate.compareTo(thatCertificate);
-        } 
-    }  
+  }
 }
--- Example/GestionEmpresaHibernate/src/company/entity/collectionsmappings/c_sortedset_one_to_many/Employee.java
+++ Example/GestionEmpresaHibernate/src/company/entity/collectionsmappings/c_sortedset_one_to_many/Employee.java
@@ -1,6 +1,7 @@
 package company.entity.collectionsmappings.c_sortedset_one_to_many;
- 
+
 import java.util.SortedSet;
+
 /**
  * <h3>SortedSet and one-to-many</h3>
  *<p>POJO class Employee which will be used to persist the objects related to
@@ -26,57 +27,61 @@ import java.util.SortedSet;
  *        in ascending element order
  *</ul>
  */
-public class Employee { 
-    private int id; 
-    private String firstName; 
-    private String lastName; 
-    private int salary; 
-    private SortedSet certificates;//<---one-to-many: one Employee many Certificates
-    
-    public Employee() {}
-    
-    public Employee(String fname, String lname, int salary) {
-        this.firstName = fname; 
-        this.lastName = lname; 
-        this.salary = salary;
-        
-    } public int getId() {
-        return id; 
-    } 
-    
-    public void setId( int id ){ 
-            this.id = id;
-    } 
-    
-    public String getFirstName() {
-        return firstName;
-    }
-    
-    public void setFirstName( String first_name ) {
-        this.firstName = first_name;
-    }
-    
-    public String getLastName() {
-        return lastName;
-    }
-    
-    public void setLastName( String last_name ) {
-        this.lastName = last_name;
-    }
-    public int getSalary() {
-        return salary;
-    }
-    
-    public void setSalary( int salary ) {
-        this.salary = salary;
-    } 
-    
-    public SortedSet getCertificates() {
-        return certificates;
-    }
-    
-    public void setCertificates( SortedSet certificates ) {
-        this.certificates = certificates;
-    }
-    
+public class Employee {
+  private int id;
+  private String firstName;
+  private String lastName;
+  private int salary;
+  private SortedSet<Certificate> certificates;//<---one-to-many: one Employee many Certificates
+
+  public Employee() {
+  }
+
+  public Employee(String fname, String lname, int salary) {
+    this.firstName = fname;
+    this.lastName = lname;
+    this.salary = salary;
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String first_name) {
+    this.firstName = first_name;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String last_name) {
+    this.lastName = last_name;
+  }
+
+  public int getSalary() {
+    return salary;
+  }
+
+  public void setSalary(int salary) {
+    this.salary = salary;
+  }
+
+  public SortedSet<Certificate> getCertificates() {
+    return certificates;
+  }
+
+  public void setCertificates(SortedSet<Certificate> certificates) {
+    this.certificates = certificates;
+  }
+
 }
