471e8026a1da47f8fd0ef643eb8d9ed8a8167487
Pedrogf03 <bleikerpro23@gmail.com>
2024-01-23 17:04:22 +0100

    23 1 24

--- Unidad1/Recuperacion/EJ11/src/ManageEmployee.java
+++ Unidad1/Recuperacion/EJ11/src/ManageEmployee.java
@@ -42,7 +42,7 @@ public class ManageEmployee {
           String newEmployeeName = sc.nextLine();
           System.out.print("Introduzca el departamento del nuevo empleado: ");
           String newDepartment = sc.nextLine();
-          System.out.print("Introduzca el ciudad del nuevo empleado: ");
+          System.out.print("Introduzca la ciudad del nuevo empleado: ");
           String newCity = sc.nextLine();
           System.out.print("Introduzca el salario del nuevo empleado: ");
           double newSalary = Double.parseDouble(sc.nextLine());
@@ -54,12 +54,21 @@ public class ManageEmployee {
           deleteEmployee(deleteEmpName, f);
           break;
         case 3:
-          //TODO: Realizar modificacion
+          System.out.print("Introduzca el nombre del empleado: ");
+          String updateEmployeeName = sc.nextLine();
+          System.out.print("Introduzca el nuevo departamento del empleado: ");
+          String updateDepartment = sc.nextLine();
+          System.out.print("Introduzca la nueva ciudad del empleado: ");
+          String updateCity = sc.nextLine();
+          System.out.print("Introduzca el nuevo salario del empleado: ");
+          double updateSalary = Double.parseDouble(sc.nextLine());
+          updateEmployee(updateEmployeeName, updateDepartment, updateCity, updateSalary, f);
           break;
         case 4:
-          //TODO: Realizar consulta
+          System.out.print("Introduzca el del nuevo empleado: ");
+          String getEmpName = sc.nextLine();
+          getEmployee(getEmpName, f);
           break;
-
         default:
           break;
       }
@@ -111,6 +120,91 @@ public class ManageEmployee {
 
     employees.delete();
     tempFile.renameTo(employees);
+  }
+
+  public static void updateEmployee(String name, String department, String city, double salary, File employees)
+      throws IOException {
+    File tempFile = new File(employees.getAbsolutePath() + ".tmp");
+
+    try (FileReader reader = new FileReader(employees);
+        FileWriter writer = new FileWriter(tempFile)) {
+
+      int c;
+      StringBuilder line = new StringBuilder();
+      while ((c = reader.read()) != -1) {
+        char caracter = (char) c;
+        if (c == '\n') {
+          if (line.toString().contains(name)) {
+            // Actualiza el registro
+            String updatedRecord = name + ";" + department + ";" + city + ";" + salary + ";0\n";
+            writer.write(updatedRecord);
+            writer.write("\n");
+          } else {
+            // Escribe el registro original
+            writer.write(line.toString());
+            writer.write("\n");
+          }
+          line = new StringBuilder();
+        } else {
+          line.append(caracter);
+        }
+      }
+      if (line.toString().contains(name)) {
+        // Actualiza el registro
+        String updatedRecord = name + ";" + department + ";" + city + ";" + salary + ";0\n";
+        writer.write(updatedRecord);
+      } else {
+        // Escribe el registro original
+        writer.write(line.toString());
+      }
+    }
+
+    employees.delete();
+    tempFile.renameTo(employees);
+  }
+
+  public static void getEmployee(String name, File employees) {
+
+    try (FileReader reader = new FileReader(employees)) {
+
+      int c;
+      StringBuilder line = new StringBuilder();
+      while ((c = reader.read()) != -1) {
+        char caracter = (char) c;
+        if (c == '\n') {
+          String finalLine = line.toString();
+          if (finalLine.contains(name)) {
+            String[] parts = finalLine.split(";");
+            if (!parts[4].equals("0\n")) {
+              System.out.println("Nombre: " + parts[0]);
+              System.out.println("Departamento: " + parts[1]);
+              System.out.println("Ciudad: " + parts[2]);
+              System.out.println("Salario: " + parts[3]);
+              System.out.println();
+            }
+          }
+          line = new StringBuilder();
+        } else {
+          line.append(caracter);
+        }
+      }
+
+      // Ultima linea
+      String finalLine = line.toString();
+      if (finalLine.contains(name)) {
+        String[] parts = finalLine.split(";");
+        if (!parts[4].equals("0\n")) {
+          System.out.println("Nombre: " + parts[0]);
+          System.out.println("Departamento: " + parts[1]);
+          System.out.println("Ciudad: " + parts[2]);
+          System.out.println("Salario: " + parts[3]);
+        }
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
